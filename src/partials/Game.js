9edb8a64b6d9973fd4477683bc543cde64bce290
Rodrigo Samayoa <jorosafi@gmail.com>
2018-02-11 21:33:26 -0600

    all code is done, but game not rendering

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,4 +1,8 @@
 import { SVG_NS } from '../settings';
+import Board from './Board';
+import KEYS from '../settings';
+import Paddle from './Paddle';
+import Ball from './Ball';
 
 export default class Game {
 
@@ -7,28 +11,62 @@ export default class Game {
 		this.width = width;
 		this.height = height;
 		this.gameElement = document.getElementById(this.element);
+		this.board = new Board(this.width, this.height);
+
+		this.paddleWidth = 8;
+		this.paddleHeight = 56;
+		this.boardGap = 10;
+
+		this.player1 = new Paddle(
+			this.height,
+			this.paddleWidth,
+			this.paddleHeight,
+			this.boardGap,
+			((this.height - this.paddleHeight) / 2),
+			KEYS.a,
+			KEYS.z
+		);
+
+		this.player2 = new Paddle(
+			this.height,
+			this.paddleWidth,
+			this.paddleHeight,
+			(this.width - this.boardGap - this.paddleWidth),
+			((this.height - this.paddleHeight) / 2),
+			KEYS.up,
+			KEYS.down
+		);
 	
+		this.ball = new Ball(8, this.width, this.height);
+
+		document.addEventListener('keydown', event => {
+			switch (event.key) {
+				case KEYS.spaceBar:
+					this.pause = !this.pause;
+					break;
+			}
+		});
 		// Other code goes here...
 	}
 
 	render() {
 		// More code goes here...
+		this.gameElement.innerHTML = ''; 
+
 		let svg = document.createElementNS(SVG_NS, 'svg');
 		svg.setAttributeNS(null, 'width', this.width);
 		svg.setAttributeNS(null, 'height', this.height);
 		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
 		this.gameElement.appendChild(svg);
-		this.gameElement.innerHTML = '';
+
+		// this.board.render(svg);
+		// this.player1.render(svg);
+		// this.player2.render(svg);
+		// this.ball.render(svg, this.player1, this.player2);
+		// if (this.pause) {
+		// 	return;
+		// }
+		// this.score1.render(svg, this.player1.score);
+		// this.score2.render(svg, this.player2.score);
 	}
-
-}
-
-export default class Board {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-  render() {
-    //...
-  }
 }
